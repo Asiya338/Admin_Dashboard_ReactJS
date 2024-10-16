69acfa14dc767d0337df1589489cd833af11e1d8
Asiya <asiyacse504@gmail.com>
2024-10-16 04:22:16 -0700

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-20 mb-5">
+      <p
+        className="text-gray-700 text-xl 
+      text-center m-24   dark:text-gray-200"
+      >
+        © 2022 All rights reserved by Shoppy.com
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
